df3160e0589a00d350db2ecb0e79873be7074185
Jordan Dalton <jordan.8474@gmail.com>
2018-12-07 12:54:55 +0000

    Implemented udp_subscriber to application and updated multiplier values
    Application now uses the UDP subscriber class.
    
    It sends the data over a configurable port on the local machine.
    
    The multiplier values have also been updated to values that I feel work best for opentrack
    .

--- Kinect-2/main.cpp
+++ Kinect-2/main.cpp
@@ -3,6 +3,7 @@
 
 #include "logger_subscriber.h"
 #include "normalized_logger_subscriber.h"
+#include "udp_subscriber.h"
 
 #include "head_tracker.h"
 
@@ -16,12 +17,37 @@
 
 #include "yal.h"
 
-int main() {
+int main(int argsc, char* argsv[]) {
 	__yal_init_logger();
 	__yal_set_lvl_info();
 
+	WSADATA wsaData;
+	int iResult = WSAStartup(MAKEWORD(2, 2), &wsaData);
+
+	if (iResult != 0) 
+	{
+		printf("WSAStartup failed with error: %d\n", iResult);
+		WSACleanup();
+		return 0;
+	}
+
+	// The port for the UDP subscriber will be the first argument passed to the application
+	int port = 4444;
+	if (argsc >= 2) 
+	{
+		port = atoi(argsv[1]);
+	}
+	else 
+	{ // Tell them they can change the UDP port
+		__yal_log(__YAL_INFO, "You can change the UDP port by passing a number as the first argument to this application.\n", port);
+	}
+
+	// Tell them what port it's listening on
+	__yal_log(__YAL_INFO, "UDP Subsciber will be sending data on port %i\n", port);
 	head_tracker *ht = new head_tracker();
 	ht->setup();
+
+	udp_subscriber *us = new udp_subscriber(port);
 	
 	head_data_filter *hdf = new head_data_filter();
 	ht->add_subscriber(hdf);
@@ -34,20 +60,19 @@ int main() {
 
 	head_data_normalizer *hdn = new head_data_normalizer(ahp);
 	hds->add_subscriber(hdn);
-
-	head_data_multiplier *hdm = new head_data_multiplier(0.75, 0.75, 0.75, 0.75, 0.75, 0.75);
+	//hds->add_subscriber(us);
+	
+	// These seem to be the best values for use with opentrack. Maybe this should be configurable?
+	head_data_multiplier *hdm = new head_data_multiplier(5,5,5,70,45,25);
 	hdn->add_subscriber(hdm);
-
+	
 	normalized_logger_subscriber *nls = new normalized_logger_subscriber("");
 	logger_subscriber *ls = new logger_subscriber("");
 
+	hdm->add_subscriber(us);
+	
 	hdm->add_subscriber(nls);
 	//ahp->add_subscriber(ls);
-
-
-
-
-
 
 	// logger_subscriber *ls1 = new logger_subscriber();
 	// logger_subscriber *ls2 = new logger_subscriber();
